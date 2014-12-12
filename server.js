15d6c83c1652178a2fb69ac72b6cb9eed81063b3
galicians <galicians@gmail.com>
2014-12-12 21:56:09 +0000

    node server in place serving index.html

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var path = require('path');
 
 
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/public');
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function(request, response) {
+    response.render('index.html')
+});
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
